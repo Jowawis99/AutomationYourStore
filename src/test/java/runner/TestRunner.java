e6dd670e8b55fde987731ed3eb033558ed204f1d
jowawis99 <johao.valen99@gmail.com>
2025-04-13 22:14:17 -0500

    Configurando proyecto con JenkinsFile 5

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -10,7 +10,7 @@ import pages.BasePage;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/features",
         glue = "steps",
-        plugin = {"pretty","html:target/cucumber-reports"}
+        publish = true
 )
 public class TestRunner {
     @AfterClass
